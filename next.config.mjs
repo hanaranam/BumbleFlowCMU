834a52877c3ab74b895e99608321f73b3ddb6a2d
Hanara Nam <hanaranam@Hanaras-MacBook-Pro.local>
2026-03-03 11:12:03 -0500

    Fixed page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+const repo = "BumbleFlowCMU";
+const isGithubPagesBuild = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPagesBuild ? `/${repo}` : "",
+  assetPrefix: isGithubPagesBuild ? `/${repo}/` : "",
 };
 
 export default nextConfig;
